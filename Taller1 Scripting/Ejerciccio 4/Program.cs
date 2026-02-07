ccfefad7a470e52be8b22f98db73f2320abc5ff3
Mariana Mejia 🪼 <marianamejiagallego@gmail.com>
2026-02-06 20:12:20 -0500

    ejercicio 4
    ejercicio 4

---
+++ Taller1 Scripting/Ejerciccio 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class AbstractSample
+{   public virtual void PrintMessage()
+    {
+        Console.WriteLine("Mensaje desde AbstractSample");
+    }
+}
+
+class MessageManager
+{
+    public void ManageMessages()
+    {
+     
+        AbstractSample sample1 = new AbstractSample();
+        AbstractSample sample2 = new AbstractSample();
+
+ 
+        sample1.PrintMessage();
+        sample2.PrintMessage();
+    }
+
+
+    static void Main(string[] args)
+    {
+        MessageManager manager = new MessageManager();
+        manager.ManageMessages();
+    }
+}
