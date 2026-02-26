61c2994607276625d771e82cfafbf44f6d262b45
Ayepes2402 <andrea.yepese@upb.edu.co>
2026-02-25 20:41:58 -0500

    Refactor EmpleadoPorHoras class with constructors

--- Taller 2 Scripting/EmpleadoPorHoras.cs
+++ Taller 2 Scripting/EmpleadoPorHoras.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace Taller_2_Scripting
 {
-    internal class EmpleadoPorHoras : Empleado
+    public class EmpleadoPorHoras : Empleado
     {
+        private int horasTrabajadas;
+        private decimal valorHora;
+
+        
+        public EmpleadoPorHoras(int id, string nombre, int horasTrabajadas, decimal valorHora)
+            : base(id, nombre)
+        {
+            this.horasTrabajadas = horasTrabajadas;
+            this.valorHora = valorHora;
+        }
+
+
+        public EmpleadoPorHoras(int id, string nombre)
+            : base(id, nombre)
+        {
+            horasTrabajadas = 0;
+            valorHora = 0;
+        }
+
+        public override decimal CalcularSalario()
+        {
+            return horasTrabajadas * valorHora;
+        }
+
+        public override void MostrarInformacion()
+        {
+            base.MostrarInformacion();
+            Console.WriteLine("Tipo: Por Horas");
+            Console.WriteLine($"Salario: {CalcularSalario()}");
+            Console.WriteLine("------------------------");
+        }
     }
 }
