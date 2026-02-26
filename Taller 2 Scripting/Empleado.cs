2170d15fbc52e294d1b9335b98b8a7eb29e9a025
Ayepes2402 <andrea.yepese@upb.edu.co>
2026-02-25 20:40:37 -0500

    Update Empleado.cs

--- Taller 2 Scripting/Empleado.cs
+++ Taller 2 Scripting/Empleado.cs
@@ -6,5 +6,21 @@ namespace Taller_2_Scripting
 {
    public abstract class Empleado : IPagable
     {
+         public int Id { get; private set; }
+         public string Nombre { get; private set; }
+
+        public Empleado(int id, string nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }
+
+        public abstract decimal CalcularSalario();
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Nombre: {Nombre}");
+        }
     }
 }
