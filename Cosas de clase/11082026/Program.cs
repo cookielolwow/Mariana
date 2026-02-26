87e631b0f99ac945c77e6e2c5d8693b1e03dd1b1
Mariana Mejia 🪼 <marianamejiagallego@gmail.com>
2026-02-25 20:18:39 -0500

    Creacion carpeta
    lol

---
+++ Cosas de clase/11082026/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Queue<string> queue = new Queue<string>();
+        Console.WriteLine($"Queue elements: {queue.Count}");
+        queue.Enqueue("First");
+        queue.Enqueue("Second");
+        queue.Enqueue("Third");
+        Console.WriteLine($"Queue elements: {queue.Count}");
+        Console.WriteLine(queue.Peek());
+        int count = queue.Count;
+
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine($"Queue elements: {queue.Count}");
+        }
+    }
+public static string FindInQueue(Queue<string> queue, string value)
+{
+    string result = null;
+    bool foundstring = false;
+        while (!foundstring && queue.Count > 0)
+        {
+            result = (queue.Dequeue());
+            foundstring = result.Equals(value);
+        }
+      
+return result;
+}
+
+}
